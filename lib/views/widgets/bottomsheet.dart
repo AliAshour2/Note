b9128716f415544caeb494fa34f7fed695aa68da
AliAshour2 <aliashour468@gmail.com>
2023-10-13 00:10:40 +0300

    add fields to bottom sheet

--- lib/views/widgets/bottomsheet.dart
+++ lib/views/widgets/bottomsheet.dart
@@ -10,8 +10,14 @@ class AddNoteBottomSheet extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Column(
         children: [
-            SizedBox(height: 32,),
-            CustomTextField(),
+          SizedBox(
+            height: 32,
+          ),
+          CustomTextField(
+            hint: "title",
+          ),
+          SizedBox(height: 16,),
+          CustomTextField(hint: "Content" , maxLines: 5,)
         ],
       ),
     );
--- lib/views/widgets/custom_text_filed.dart
+++ lib/views/widgets/custom_text_filed.dart
@@ -2,28 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:note/constants.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
+  const CustomTextField({super.key,  required this.hint,  this.maxLines =1});
+
+  final String hint;
+  final int maxLines;
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       cursorColor: kPrimaryColor,
+      maxLines: maxLines,
       decoration: InputDecoration(
-        hintText: "Title",
-        
+        hintText: hint,
         border: buildBorder(),
         enabledBorder: buildBorder(),
         focusedBorder: buildBorder(kPrimaryColor),
       ),
     );
-    
   }
 
   OutlineInputBorder buildBorder([color]) {
     return OutlineInputBorder(
-        borderRadius: BorderRadius.circular(16),
-        borderSide: BorderSide(color : color ??Colors.white),
-        
-      );
+      borderRadius: BorderRadius.circular(16),
+      borderSide: BorderSide(color: color ?? Colors.white),
+    );
   }
 }
