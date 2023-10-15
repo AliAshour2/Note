4b77af466bc653e9567f2925cc3e63980b1e1744
AliAshour2 <aliashour468@gmail.com>
2023-10-15 20:11:49 +0300

    fininshin buttom sheet

--- lib/views/widgets/bottomsheet.dart
+++ lib/views/widgets/bottomsheet.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:note/constants.dart';
 import 'package:note/views/widgets/custom_text_filed.dart';
+import 'package:note/views/widgets/cutom_buttom.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
@@ -8,17 +10,28 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 32,
-          ),
-          CustomTextField(
-            hint: "title",
-          ),
-          SizedBox(height: 16,),
-          CustomTextField(hint: "Content" , maxLines: 5,)
-        ],
+      child: SingleChildScrollView(
+        
+        child: Column(
+          children: [
+            SizedBox(
+              height: 32,
+            ),
+            CustomTextField(
+              hint: "title",
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextField(
+              hint: "Content",
+              maxLines: 5,
+            ),
+            SizedBox(height: 16,),
+            CustomButton(),
+            SizedBox(height: 8,)
+          ],
+        ),
       ),
     );
   }
