e059f41ebfff6f681a8a48a0e1cc835ed2c66e4d
AliAshour2 <aliashour468@gmail.com>
2023-10-16 12:14:55 +0300

    taking inoput validate it

--- lib/views/widgets/cutom_buttom.dart
+++ lib/views/widgets/cutom_buttom.dart
@@ -3,19 +3,22 @@ import 'package:flutter/material.dart';
 import 'package:note/constants.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
-
+  const CustomButton({Key? key, this.onTap}) : super(key: key);
+  final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 55,
-      decoration: BoxDecoration(
-        color: kPrimaryColor,
-        borderRadius: BorderRadius.circular(8)
-      ),
-      child: const Center(
-        child: Text("Add" , style: TextStyle(fontSize: 18 , ),),
+    return GestureDetector(
+      onTap:onTap,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 55,
+        decoration: BoxDecoration(
+          color: kPrimaryColor,
+          borderRadius: BorderRadius.circular(8)
+        ),
+        child: const Center(
+          child: Text("Add" , style: TextStyle(fontSize: 18 , ),),
+        ),
       ),
     );
     
